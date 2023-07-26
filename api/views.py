9ad2909c60f4b8fa72f181db8e38d3b813514ce5
djumanov <diyorbekjumanov1120@gmail.com>
2023-07-26 15:58:53 +0500

    task view

--- api/views.py
+++ api/views.py
@@ -55,4 +55,86 @@ class UsersView(View):
             return JsonResponse({'status': 'ok'})
         except User.DoesNotExist:
             return JsonResponse({'error': 'User not found'}, status=404)
+
+
+
+class TasksView(View):
+    def get(self, request: HttpRequest, pk=None) -> JsonResponse:
+        data = request.body.decode('utf-8')
+        data = json.loads(data)
+
+        try:
+            user = User.objects.get(pk=data['user_id'])
+        except User.DoesNotExist:
+            return JsonResponse({'error': 'User not found'}, status=404)
+        except KeyError:
+            return JsonResponse({'error': 'Invalid data'}, status=400)
+            
+        if pk is None:
+            tasks = Task.objects.filter(user=user)
+            tasks_list = [model_to_dict(task, fields=['id', 'title', 'completed']) for task in tasks]
+            return JsonResponse(tasks_list, safe=False)
+        else:
+            try:
+                task = Task.objects.get(pk=pk, author=user)
+                task_dict = model_to_dict(task, fields=['id', 'title', 'completed'])
+                return JsonResponse(task_dict)
+            except Task.DoesNotExist:
+                return JsonResponse({'error': 'Task not found'}, status=404)
+
+    def post(self, request: HttpRequest) -> JsonResponse:
+        data = request.body.decode('utf-8')
+        data = json.loads(data)
+
+        try:
+            user = User.objects.get(pk=data['user_id'])
+            task = Task.objects.create(title=data['title'], description=data.get('description', ''), author=user)
+            task.save()
+            return JsonResponse(model_to_dict(task, fields=['id', 'title', 'completed']))
+        except User.DoesNotExist:
+            return JsonResponse({'error': 'User not found'}, status=404)
+        except KeyError:
+            return JsonResponse({'error': 'Invalid data'}, status=400)
     
+    
+    def put(self, request: HttpRequest, pk: int) -> JsonResponse:
+        data = request.body.decode('utf-8')
+        data = json.loads(data)
+
+        try:
+            user = User.objects.get(pk=data['user_id'])
+        except User.DoesNotExist:
+            return JsonResponse({'error': 'User not found'}, status=404)
+        except KeyError:
+            return JsonResponse({'error': 'Invalid data'}, status=400)
+
+        try:
+            task = Task.objects.get(id=pk, author=user)
+        except Task.DoesNotExist:
+            return JsonResponse({'error': 'Task not found'}, status=404)
+
+        task.title = data.get('title', task.title)
+        task.description = data.get('description', task.description)
+        task.completed = data.get('completed', task.completed)
+
+        task.save()
+        return JsonResponse(model_to_dict(task, fields=['id', 'title', 'completed']))
+
+    def delete(self, request: HttpRequest, pk: int) -> JsonResponse:
+        data = request.body.decode('utf-8')
+        data = json.loads(data)
+
+        try:
+            user = User.objects.get(pk=data['user_id'])
+        except User.DoesNotExist:
+            return JsonResponse({'error': 'User not found'}, status=404)
+        except KeyError:
+            return JsonResponse({'error': 'Invalid data'}, status=400)
+
+        try:
+            task = Task.objects.get(author=user, id=pk)
+            task.delete()
+            return JsonResponse({'status': 'deleted task'}, status=204)
+        except Task.DoesNotExist:
+            return JsonResponse({'error': 'Task not found'}, status=404)
+        
