bfbe0f11d2c37b9471c907e827779fa6fc29c412
djumanov <diyorbekjumanov1120@gmail.com>
2023-07-26 16:36:37 +0500

    allowed host

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import UsersView
+from .views import UsersView, TasksView
 
 urlpatterns = [
     path('users/', UsersView.as_view()),
     path('users/<int:pk>/', UsersView.as_view()),
+    path('tasks/', TasksView.as_view()),
+    path('tasks/<int:pk>/', TasksView.as_view()),
 ]
