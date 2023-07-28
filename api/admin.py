307756aaa7ec32f9a95d3497d11e69c2c2ee5a31
djumanov <diyorbekjumanov1120@gmail.com>
2023-07-28 15:38:07 +0500

    model admin

--- api/admin.py
+++ api/admin.py
@@ -4,4 +4,13 @@ from .models import Task
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'description', 'completed', 'created_at', 'updated_at')
+    list_display = ('id', 'title', 'description', 'completed', 'created_at', 'updated_at', 'author')
+    list_display_links = ('id', 'title', 'created_at')
+    list_filter = ('completed', 'created_at', 'updated_at')
+    search_fields = ('title', 'description', 'id')
+    ordering = ('-created_at', '-updated_at')
+    date_hierarchy = 'created_at'
+
+    def author(self, obj):
+        return obj.author.username
+        
